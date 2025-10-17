0ee43ad56d8bbf043b57e77cca5dbd535a70254d
bdpsoft <bdpsoft@gmail.com>
2025-10-17 14:06:31 +0200

    ubacen storybook

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+
+
+/** @type { import('storybook-solidjs-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-docs"
+  ],
+  "framework": {
+    "name": "storybook-solidjs-vite",
+    "options": {}
+  }
+};
+export default config;
